0103ad3df0737f4900f0db79a8b70422b79ec0d8
Seokyoon Kim <isdebrave@gmail.com>
2024-01-12 08:40:12 +0900

    cors

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,6 +1,6 @@
 export const backendUrl =
   process.env.NODE_ENV === "production"
-    ? "http://isdebrave-twitter-clone.shop"
+    ? "http://api.isdebrave-twitter-clone.shop"
     : "http://localhost:8080";
 
 // export const backendUrl = "http://localhost:8080";
