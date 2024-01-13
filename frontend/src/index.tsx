9bdd387ff96036a92c328e801495e1060cbf06fe
Seokyoon Kim <isdebrave@gmail.com>
2024-01-13 13:04:55 +0900

    frontend/dist

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 import { store } from "./redux/store";
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "http://54.172.219.207";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
