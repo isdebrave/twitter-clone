76996f423670b4fb2f20f22bc8c77a748ea74543
Seokyoon Kim <isdebrave@gmail.com>
2024-01-22 14:34:46 +0900

    update

--- frontend/src/helpers/image.ts
+++ frontend/src/helpers/image.ts
@@ -6,6 +6,7 @@ export const src = (image: string) => {
     return "/images/anonymous.jpg";
   }
 
+  console.log(image);
   return "https://source.unsplash.com/random/300×300";
 
   // return image;
