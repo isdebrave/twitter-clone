780d4239acae66921ebb7bb6f243eda883970965
Seokyoon Kim <isdebrave@gmail.com>
2024-01-31 14:37:13 +0900

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -32,7 +32,7 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET!,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
     cookie: {
       httpOnly: true,
--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -307,6 +307,7 @@ export const login = async (
     }
 
     req.session.meId = user.id;
+    console.log("login", req.session);
 
     return res.status(200).json();
   } catch (error) {
