22eabfcd54a31fb5e90391ffb3346a29bd5f12a9
Seokyoon Kim <isdebrave@gmail.com>
2024-01-31 15:22:33 +0900

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,8 +23,6 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan("combined"));
-app.use(hpp());
-app.use(helmet());
 app.use(
   cors({ origin: "https://isdebrave-twitter-clone.shop", credentials: true })
 );
@@ -41,6 +39,8 @@ app.use(
     },
   })
 );
+app.use(hpp());
+app.use(helmet());
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(morgan("combined"));
@@ -65,18 +65,18 @@ app.use(
 // } else {
 //   app.use(morgan("dev"));
 //   app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET!,
-//     resave: false,
-//     saveUninitialized: false,
-//     store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
-//     cookie: {
-//       httpOnly: true,
-//       secure: false,
-//     },
-//   })
-// );
+//   app.use(
+//     session({
+//       secret: process.env.SESSION_SECRET!,
+//       resave: false,
+//       saveUninitialized: false,
+//       store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+//       cookie: {
+//         httpOnly: true,
+//         secure: false,
+//       },
+//     })
+//   );
 // }
 
 app.get("/", (req, res) => {
